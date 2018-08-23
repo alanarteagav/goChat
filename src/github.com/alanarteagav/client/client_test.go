ae3bd0097665ba80d2865f56eebe8865bc8a4b85
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-08-23 18:33:02 -0500

    Adding unit tests to client_test and server_test

--- src/github.com/alanarteagav/client/client_test.go
+++ src/github.com/alanarteagav/client/client_test.go
@@ -1,1 +1,35 @@
 package client
+
+import "testing"
+
+func TestNewStudent(t *testing.T)  {
+    t.Error("TestNewStudent FAILED")
+}
+
+func TestSetUsername(t *testing.T) {
+    t.Error("TestSetUsername FAILED")
+}
+
+func TestListen(t *testing.T) {
+    t.Error("TestListen FAILED")
+}
+
+func TestRead(t *testing.T) {
+    t.Error("TestRead FAILED")
+}
+
+func TestWrite(t *testing.T) {
+    t.Error("TestWrite FAILED")
+}
+
+func TestLogIn(t *testing.T)  {
+    t.Error("TestLogIn FAILED")
+}
+
+func TestLogOut(t *testing.T) {
+    t.Error("TestLogOut FAILED")
+}
+
+func TestAskForChatroom(t *testing.T) {
+    t.Error("TestAskForChatroom FAILED")
+}
--- src/github.com/alanarteagav/server/server_test.go
+++ src/github.com/alanarteagav/server/server_test.go
@@ -1,1 +1,23 @@
 package server
+
+import "testing"
+
+func TestAcceptClient(t *testing.T) {
+    t.Error("TestAcceptClient FAILED")
+}
+
+func TestClientLogOut(t *testing.T) {
+    t.Error("TestClientLogOut FAILED")
+}
+
+func TestDistribute(t *testing.T) {
+    t.Error("TestDistribute FAILED")
+}
+
+func TestWrite(t *testing.T) {
+    t.Error("TestWrite FAILED")
+}
+
+func TestListen(t *testing.T) {
+    t.Error("TestListen FAILED")
+}
