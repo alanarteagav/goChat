461558a32722842026080b2b3c50b91642cfbf97
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-08-29 23:05:53 -0500

    Adding constants and ToChatServer() function in events file.

--- src/github.com/alanarteagav/events/events.go
+++ src/github.com/alanarteagav/events/events.go
@@ -1,1 +1,26 @@
 package events
+
+type ChatEvent string
+
+const (
+    LOG_IN  ChatEvent = "LOG_IN"
+    LOG_OUT ChatEvent = "LOG_OUT"
+    MESSAGE ChatEvent = "MESSAGE"
+    ERROR   ChatEvent = "ERROR"
+    UNDEFINED   ChatEvent = "UNDEFINED"
+)
+
+func ToChatEvent(str string) ChatEvent {
+    switch str {
+    case "MESSAGE":
+        return MESSAGE
+    case "LOG_IN":
+        return LOG_IN
+    case "LOG_OUT":
+        return LOG_OUT
+    case "ERROR":
+        return ERROR
+    default:
+        return UNDEFINED
+    }
+}
