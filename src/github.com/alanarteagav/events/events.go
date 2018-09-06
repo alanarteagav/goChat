2aa92d5775c47ca42f76430be7be84781de969d4
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-09-05 22:27:16 -0500

    New chat events, adding comments to 'events' file.

--- src/github.com/alanarteagav/events/events.go
+++ src/github.com/alanarteagav/events/events.go
@@ -1,15 +1,24 @@
+// ChatEvent constants, to be used in the protocol between the
+// client and the server.
 package events
 
 type ChatEvent string
 
+// ChatEvent constants, to be used in the protocol between the
+// client and the server.
 const (
     LOG_IN  ChatEvent = "LOG_IN"
     LOG_OUT ChatEvent = "LOG_OUT"
     MESSAGE ChatEvent = "MESSAGE"
+    CREATE_CHATROOM ChatEvent = "CREATE_CHATROOM"
+    JOIN_CHATROOM ChatEvent = "JOIN_CHATROOM"
+    LEAVE_CHATROOM ChatEvent = "LEAVE_CHATROOM"
+    MESSAGE_CHATROOM ChatEvent = "MESSAGE_CHATROOM"
     ERROR   ChatEvent = "ERROR"
     UNDEFINED   ChatEvent = "UNDEFINED"
 )
 
+// Function that converts strings into ChatEvent constants.
 func ToChatEvent(str string) ChatEvent {
     switch str {
     case "MESSAGE":
@@ -18,6 +27,14 @@ func ToChatEvent(str string) ChatEvent {
         return LOG_IN
     case "LOG_OUT":
         return LOG_OUT
+    case "CREATE_CHATROOM":
+        return CREATE_CHATROOM
+    case "JOIN_CHATROOM":
+        return JOIN_CHATROOM
+    case "LEAVE_CHATROOM":
+        return LEAVE_CHATROOM
+    case "MESSAGE_CHATROOM":
+        return MESSAGE_CHATROOM
     case "ERROR":
         return ERROR
     default:
