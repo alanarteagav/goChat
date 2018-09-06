fd497a713e57f2960b98b985bd7b2b735b544c6b
Alan Arteaga <alanarteagav@ciencias.unam.mx>
2018-09-06 13:29:08 -0500

    Fixing bash script, replacing goClient.go for goClient.c, new glade file

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 # Bash script for building goChat
+export CGO_ENABLES="0"
 export GOPATH=$(pwd)/
 export GOBIN=$(pwd)/bin
 go build ./...
 go install ./...
+gcc -o bin/goClient ./src/github.com/alanarteagav/goClient/goClient.c -Wall `pkg-config --cflags --libs gtk+-3.0` -export-dynamic
